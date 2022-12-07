6d662748e29c3837ed37dde1da114de2bee6ca66
Ryan da Silva de Castro Pereira <ryandasilva1008@gmail.com>
2022-12-07 10:20:37 -0300

    criando o projeto

---
+++ src/componets/Header.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const Content = styled.div`
+    width: 100vw;
+    height: 50px;
+    background: grey;
+`
+
+export default function Header(props){
+    return(
+        <Content>
+        </Content>
+    )
+}
